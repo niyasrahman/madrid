1cf11c3c415aa9747e965b144b764a6857c4685c
Asim K T <asim@quintype.com>
2017-09-12 13:41:45 +0530

    Change home-page-data-loader

--- app/isomorphic/components/pages/home.jsx
+++ app/isomorphic/components/pages/home.jsx
@@ -3,14 +3,21 @@ const React = require("react");
 const { StoryGrid } = require("../story-grid.jsx");
 const { TwoColOneAd } = require("../layout/two-col-one-ad.jsx");
 
+
+const templates = {
+  'wh-block-featured': TwoColOneAd
+}
+
+function getTemplate(designTemplate){
+  return templates[designTemplate];
+}
+
+
 class HomePage extends React.Component {
 
-
   render() {
+    console.log(this.props.data.homeCollection)
     return <div>
-      {this.props.data.collections.map((collection) =>
-        <TwoColOneAd collection={collection} key={collection.id}/>
-      )}
     </div>;
   }
 }
--- app/server/data-loaders/home-page-data.js
+++ app/server/data-loaders/home-page-data.js
@@ -1,10 +1,36 @@
+_ = require("lodash");
 const {Story, client} = require("quintype-toddy-libs/server/api-client");
 
-exports.loadHomePageData = function loadHomePageData(){
+exports.loadHomePageData = function loadHomePageData() {
+  let placeholderCollectionSlugs = [];
   return client.getCollectionBySlug('home', {})
-    .then(collection => {
-      return {
-        collections: collection.items
-      }
+    .then(homeCollection => {
+      placeholderCollectionSlugs = childCollectionSlugsFromCollection(homeCollection);
+      return makeBulkRequest(placeholderCollectionSlugs);
+    })
+    .then(allCollections => {
+      return {'homeCollection': _.sortBy(allCollections.results, allCollections.parentCollection)};
     });
+}
+
+function childCollectionSlugsFromCollection(collection) {
+  return _(collection.items)
+    .filter(item => item.type === 'collection')
+    .map(item => item.slug)
+    .value();
+}
+
+function createCollectionBulkRequest(slugs) {
+  return _.reduce(slugs, function(acc, slug) {
+    acc[slug] = {"_type": "collection", "slug": slug, "limit": "8"}
+    return acc
+  }, {});
+}
+
+function makeBulkRequest(slugs) {
+  var requestPayload = createCollectionBulkRequest(_.flatten(slugs));
+  return client.getInBulk({requests: requestPayload})
+  .then(bulkResponse => {
+    return {'results': bulkResponse.results};
+  })
 }
