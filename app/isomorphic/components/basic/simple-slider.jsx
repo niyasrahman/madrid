6c7472304ac8bbef5d8c66e12110ade3b287d917
Asim K T <asim@quintype.com>
2017-10-13 12:31:20 +0530

    [SimpleSlider] Extracted to indedependet component

--- app/isomorphic/components/basic/simple-slider.jsx
+++ app/isomorphic/components/basic/simple-slider.jsx
@@ -2,7 +2,8 @@ const React = require("react");
 const Slider = require("react-slick").default;
 
 const { Link } = require("quintype-toddy-libs/components/link");
-const backgroundImage = require('../../../assets/images/nyc-city.jpg');
+const { ResponsiveImage } = require("quintype-toddy-libs/components/responsive-image");
+
 
 function SimpleSlider(props) {
   const settings = {
@@ -20,10 +21,10 @@ function SimpleSlider(props) {
         }
       ]
     };
-  return !props.stories ? null : <div className="must__reads">
+  return !props.stories ? null : <div className="simple-slider">
       <Slider {...settings}>
         {props.stories.map((item) =>
-          <div key={item.id} className="must__reads__slider">
+          <div key={item.id}>
             <SliderItem item={item} />
           </div>
         )}
@@ -34,15 +35,18 @@ function SimpleSlider(props) {
 function SliderItem(props) {
   {/* The `story` can be an item from items of a collection or a story itself.
    assigning it accordinlgy.*/}
-  const story = props.item.type === 'story' && props.item.story ? props.item.story : props.item;
+  const story = props.item && props.item.type === 'story' && props.item.story ? props.item.story : props.item;
 
   return (
-    <div className="must__reads__slide">
-      <img src={backgroundImage} alt="" />
-      <div className="slide__content">
+    <div className="simple-slider__item">
+      <figure>
+        <ResponsiveImage slug={story["hero-image-s3-key"]} metadata={story["hero-image-metadata"]}
+          aspectRatio={[16,3]}/>
+      </figure>
+      <div className="simple-slider__content">
         <p>Must Reads</p>
         <h2>{story.headline}</h2>
-        <Link href={"/" + (story['parent-collection'] ? story['generated-slug'] : story.slug) } className="slide__more button button--link" >
+        <Link href={"/" + (story['parent-collection'] ? story['generated-slug'] : story.slug) } className="simple-slider__link" >
           read story
         </Link>
       </div>
