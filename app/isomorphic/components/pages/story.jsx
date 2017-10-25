30d97b88568f879387618e9393d7c9884e2d6468
Kavya <kavya@quintype.com>
2017-10-25 16:19:41 -0700

    Initial changes for including live blog story
    1. checks for story type and render respective template in story.jsx
    2. storyPageCard is made reusable and extracted out from blank template file
    3. added live-blog jsx file

--- app/isomorphic/components/pages/story.jsx
+++ app/isomorphic/components/pages/story.jsx
@@ -1,6 +1,7 @@
 const React = require("react");
 
 const { BlankStory } = require("../story-templates/blank.jsx");
+const { LiveBlog } = require("../story-templates/live-blog.jsx");
 const { NavigationComponent } = require("../navigation-component.jsx");
 const { Footer } = require('../layout/footer.jsx')
 
@@ -35,7 +36,10 @@ class StoryPage extends React.Component {
     };
     return (<div>
         <NavigationComponent {...navbarConfig}/>
-        <BlankStory story={this.props.data.story} relatedStories={this.props.data.relatedStories['related-stories']}/>
+        { (this.props.data.story['story-template'] === "live-blog")?
+          <LiveBlog story={this.props.data.story} /> :
+          <BlankStory story={this.props.data.story} relatedStories={this.props.data.relatedStories['related-stories']}/>
+        }
         <Footer links={staticLinks}/>
       </div>
     )
---
+++ app/isomorphic/components/story-templates/live-blog.jsx
@@ -0,0 +1,9 @@
+const React = require("react");
+
+function LiveBlog(props) {
+  return <div className="story-grid">
+    Live blog story  here
+  </div>
+}
+
+exports.LiveBlog = LiveBlog;
