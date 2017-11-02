abc98a26601a86afb76aead959a8946883cdb337
SoorajBala <sooraj.bala@gmail.com>
2017-11-02 16:50:19 +0530

    [WIP]

--- app/isomorphic/components/pages/section.jsx
+++ app/isomorphic/components/pages/section.jsx
@@ -6,7 +6,7 @@ const { Footer } = require('../layout/footer.jsx')
 const { FullscreenMediaList } = require("../layout/fullscreen-media-list.jsx");
 const { ThreeCol } = require("../layout/three-col.jsx");
 const { ThreeStoryCards } = require("../layout/three-story-cards.jsx");
-const { Seven } = require("../layout/seven.jsx");
+const { OneStoryCardSixStoryList } = require("../layout/onestorycard-sixstorylist.jsx");
 const { CardGroup } = require("../layout/card-group.jsx");
 
 class SectionPage extends React.Component {
@@ -40,7 +40,7 @@ class SectionPage extends React.Component {
       <NavigationComponent {...navbarConfig}/>
       <CardGroup stories={this.props.data.collection.items.slice(0,4)} config= {config}/>
       <ThreeStoryCards stories={this.props.data.collection.items.slice(0,3)} config= {config}/>
-      <Seven stories={this.props.data.collection.items.slice(0,7)} config= {config}/>
+      <OneStoryCardSixStoryList stories={this.props.data.collection.items.slice(0,7)} config= {config}/>
       <ThreeCol stories={this.props.data.collection.items.slice(0,7)} config= {config}/>
       <FullscreenMediaList stories={this.props.data.collection.items.slice(4)} config= {config}/>
       <Footer links={staticLinks}/>
