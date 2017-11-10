bbc8ba408f077c9b8983cbdddafa2e87fb55d812
Kavya <kavya@quintype.com>
2017-11-09 17:51:27 -0800

    changes for breaking news.

--- app/isomorphic/components/breaking-news-view.js
+++ app/isomorphic/components/breaking-news-view.js
@@ -1,15 +1,37 @@
 const React = require("react");
+const { Link } = require("quintype-toddy-libs/components/link");
+const Slider = require("react-slick").default;
 
 function BreakingNewsView(props) {
+  const settings = {
+      dots: false,
+      infinite: true,
+      speed: 1000,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      arrows: false,
+      fade: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
   if(props.breakingNews.length == 0) {
     return <span/>;
   }
-  return <ul className="breaking-news">
-    {props.breakingNews.map(story => breakingNewsItem(story))}
-  </ul>;
+  return <div className="breaking-news">
+    <span className="breaking-news__heading">Breaking News</span>
+    <div className="breaking-news__carousel">
+      <Slider {...settings}>
+        {props.breakingNews.map(story => breakingNewsItem(story))}
+      </Slider>
+    </div>
+  </div>;
 
   function breakingNewsItem(story) {
-    return <li key={story.id}>{story.headline}</li>
+    if(story.metadata['linked-story-slug']){
+      return <Link key={story.id} href={ '/'+ story.metadata['linked-story-slug']} className="breaking-news__story">{story.headline}</Link>
+    } else {
+    return <article key={story.id} className="breaking-news__story">{story.headline}</article>
+    }
   }
 }
 
