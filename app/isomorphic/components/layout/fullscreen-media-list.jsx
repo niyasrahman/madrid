d6f6a4fd59083896228d468cf82581d92b4f4750
SoorajBala <sooraj.bala@gmail.com>
2017-11-14 15:01:32 +0530

    ads style added

--- app/isomorphic/components/layout/fullscreen-media-list.jsx
+++ app/isomorphic/components/layout/fullscreen-media-list.jsx
@@ -3,6 +3,8 @@ const React = require("react");
 const { MediaObject } = require("../basic/media-object-list.jsx")
 const { SectionName } = require("../basic/section-name.jsx");
 
+const adImage = require('../../../assets/images/qt-ad.png');
+
 function FullscreenMediaList(props) {
   const config = {
     section: !props.HideSectionName,
@@ -22,13 +24,22 @@ function FullscreenMediaList(props) {
         <SectionName inlineStyle={inlineStyle} name={props.config['collection-name']} type="large"/>
         : null
       }
+      <div className="col-8">
       {props.stories.slice(0,5)
         .filter((item) => item.type === 'story')
         .map(storyObj =>
-        <div className="col-9 fullscreen-media-list__object" key={storyObj.id}>
+        <div className="fullscreen-media-list__object" key={storyObj.id}>
           <MediaObject story={storyObj.story} config={config}/>
         </div>
       )}
+      </div>
+      <div className="col-4 ads-375x245">
+      {
+        props.adsSlot ?
+        <img src={adImage} alt="Ad"/>
+        : null
+      }
+      </div>
     </div>
   </div>
 }
