f7f5bb355ed47cc9ea406ba94f68eca69075c523
Tejas Dinkar <tejas@gja.in>
2017-11-23 16:44:41 +0530

    Infinite scroll

--- app/isomorphic/components/pages/story.jsx
+++ app/isomorphic/components/pages/story.jsx
@@ -4,7 +4,23 @@ const { BlankStory } = require("../story-templates/blank.jsx");
 const { LiveBlogStory } = require("../story-templates/live-blog.jsx");
 const { NavigationComponent } = require("../navigation-component.jsx");
 const { Footer } = require('../layout/footer.jsx')
+const { InfiniteStoryBase } = require('quintype-toddy-libs/components/infinite-story-base')
 
+function storyPageContent({story, index}, relatedStories) {
+  if(story['story-template'] == 'live-blog') {
+    return <LiveBlogStory story={story} />;
+  } else {
+    return <BlankStory story={story} relatedStories={index == 0 ? relatedStories : []}/>
+  }
+}
+
+const FIELDS = "id,headline,slug,url,hero-image-s3-key,hero-image-metadata,first-published-at,last-published-at,alternative,published-at,author-name,author-id,sections,story-template,cards";
+function storyPageLoadStories(pageNumber) {
+  // Replace this with your logic for loading stories
+  return global.superagent
+           .get("/api/v1/stories", {fields: FIELDS, limit:5, offset:5*pageNumber})
+           .then(response => response.body.stories);
+}
 
 class StoryPage extends React.Component {
   constructor(props) {
@@ -36,10 +52,11 @@ class StoryPage extends React.Component {
     };
     return (<div>
         <NavigationComponent {...navbarConfig}/>
-        { (this.props.data.story['story-template'] === "live-blog")?
-          <LiveBlogStory story={this.props.data.story} /> :
-          <BlankStory story={this.props.data.story} relatedStories={this.props.data.relatedStories}/>
-        }
+        <InfiniteStoryBase {...this.props}
+                            render={(storyProps) => storyPageContent(storyProps, this.props.data.relatedStories)}
+                            loadStories={storyPageLoadStories}
+                            onStoryFocus={(story) => console.log(`Story In View: ${story.headline}`)}
+                            onInitialStoryFocus={(story) => console.log(`Do Analytics ${story.headline}`)} />
         <Footer links={staticLinks}/>
       </div>
     )
