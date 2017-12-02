e33ca3e06fee6e06422afa07b4585df6fce19a44
Tejas Dinkar <tejas@gja.in>
2017-12-02 22:26:27 +0530

    Moving to hot reload

--- start.js
+++ start.js
@@ -1,31 +1,12 @@
-var cluster = require('cluster');
-var process = require("process");
+const {startApp} = require("@quintype/framework/server/start");
 
 const PUBLIC_PATH='/ace/assets/';
 const FileName = process.env.NODE_ENV == 'production' ? `${PUBLIC_PATH}[name]-[hash:20].[ext]` : `${PUBLIC_PATH}[name].[ext]`;
 
-if(cluster.isMaster) {
-  var os = require('os');
-  for (var i = 0; i < 4; i++) {
-    cluster.fork();
-  }
-  cluster.on('exit', function(worker, code, signal) {
-    console.log('worker ' + worker.process.pid + ' died');
-    cluster.fork();
-  });
-} else {
-  require('node-jsx').install();
+startApp(() => {
   require('asset-require-hook')({
     extensions: ['png', 'jpg', 'jpeg', 'svg', 'gif', 'mp4'],
     name: FileName
   });
-  var startApp = require("./app/server/server.js");
-  startApp().catch(function(e) {
-    var sleep = require("sleep-promise");
-    console.error("Worker died - Aborting");
-    console.error(e.stack);
-    return new Promise((resolve) => resolve(cluster.worker.disconnect()))
-      .then(() => sleep(250))
-      .then(() => process.exit());
-  });
-}
+  return require("./app/server/app.js");
+})
