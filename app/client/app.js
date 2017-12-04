119c7a5a9531e14115fa3efb5617393807b090a5
Tejas Dinkar <tejas@gja.in>
2017-12-05 01:03:11 +0530

    StartApp

--- app/client/app.js
+++ app/client/app.js
@@ -1,33 +1,17 @@
 import css from '../../app/assets/stylesheets/application.scss';
 
-import { createQtStore } from '@quintype/framework/store/create-store';
-import { history, getRouteData, app } from '@quintype/framework/client/start';
-import { NAVIGATE_TO_PAGE } from '@quintype/components/store/actions';
 import { renderApplication } from './render'
+import { startApp } from '@quintype/framework/client/start'
+
+function enableHotReload(store) {
+  if(process.env.NODE_ENV == 'development' && module.hot) {
+    module.hot.accept('./render', () => renderApplication(store));
+  }
+}
+
+const CUSTOM_REDUCERS = {};
 
 require('whatwg-fetch');
-global.Promise = global.Promise || require("bluebird");
-global.superagent = require('superagent-promise')(require('superagent'), Promise);
-global.app = app;
-
-// This is the entry point. Ideally, unused functions will get compiled out
-function startApp() {
-  const location = global.location;
-  getRouteData(`${location.pathname}${location.search || ""}`, {config: true})
-    .then((result) => {
-      const store = createQtStore(null, result.body);
-
-      renderApplication(store);
-      history.listen(change => app.maybeNavigateTo(`${change.pathname}${change.search || ""}`, store));
-
-      if(process.env.NODE_ENV == 'development' && module.hot) {
-        module.hot.accept('./render', () => renderApplication(store));
-      }
-    });
-}
-
-startApp();
-
-if(process.env.NODE_ENV == 'production' && global.navigator.serviceWorker) {
-    global.navigator.serviceWorker.register("/service-worker.js");
-}
+startApp(renderApplication, CUSTOM_REDUCERS, {
+  enableServiceWorker: process.env.NODE_ENV == 'production'
+}).then(enableHotReload);
