da6415de6bdf9631db42df806611b21413b9028a
Asim K T <asim@quintype.com>
2017-12-05 14:17:58 +0530

    Show load more in recent stories section

--- app/isomorphic/components/layout/fullscreen-media-list.js
+++ app/isomorphic/components/layout/fullscreen-media-list.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "@quintype/components";
 
 import { MediaObject } from "../basic/media-object-list.js"
 import { SectionName } from "../basic/section-name.js";
@@ -17,7 +18,7 @@ function FullscreenMediaList(props) {
   const inlineStyle = {
     borderBottom: '4px solid ' + props.config['collection-color']
   }
-  const limit = props.config.limit || 5;
+  const limit = props.config.limit || 3;
   return <div className="fullscreen-media-list">
     <div className="fullscreen-media-list__content component-wrapper">
       {
@@ -26,14 +27,19 @@ function FullscreenMediaList(props) {
         : null
       }
       <div className="col-8">
-      {props.stories.slice(0,limit)
-        .map(storyObj => {
-          const story = storyObj && storyObj.story ? storyObj.story : storyObj;
-          return <div className="fullscreen-media-list__object" key={story.id}>
-            <MediaObject story={story} config={mediaObjectConfig}/>
-          </div>
-        }
-      )}
+        {props.stories.slice(0,limit)
+          .map(storyObj => {
+            const story = storyObj && storyObj.story ? storyObj.story : storyObj;
+            return <div className="fullscreen-media-list__object" key={story.id}>
+              <MediaObject story={story} config={mediaObjectConfig}/>
+            </div>
+          }
+        )}
+        <div className="fullscreen-media-list__load-more">
+          <Link href={'/' + props.config['collection-slug']}>
+            Load more
+          </Link>
+        </div>
       </div>
       <div className="col-4">
         {
