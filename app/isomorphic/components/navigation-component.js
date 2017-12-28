68807df06ea8aad27e4ecf0b72e03b13827c9104
Asim K T <asim@quintype.com>
2017-12-28 15:07:10 +0530

    [NavigationComponent] Hide Offcanvas menu whenever state changes

--- app/isomorphic/components/navigation-component.js
+++ app/isomorphic/components/navigation-component.js
@@ -15,27 +15,36 @@ class NavigationBase extends React.Component {
       isOffcanvasOpen: false
     }
     this.openSidemenu = this.openSidemenu.bind(this);
+    this.closeSideMenu = this.closeSideMenu.bind(this);
+  }
+  componentWillReceiveProps(newProps) {
+    !newProps.showOffcanvasMenu && this.closeSideMenu();
   }
   openSidemenu() {
     this.setState({
       isOffcanvasOpen: true
     })
   }
+  closeSideMenu() {
+    this.setState({
+      isOffcanvasOpen: false
+    })
+  }
   render() {
     return <div className="navbar">
-      <OffcanvasMenu {...this.props} isOffcanvasOpen={this.state.isOffcanvasOpen}/>
+      <OffcanvasMenu {...this.props} isOffcanvasOpen={this.state.isOffcanvasOpen} closeMenu={this.closeSideMenu}/>
       <NavBar {...this.props} openSidemenu={this.openSidemenu}/>
     </div>
   }
 }
 
 function mapStateToProps(state) {
-
   // Showing the first 5 menu items only to keep up with design.
   return {
     title: 'Madrid',
     menu: _.get(state, ["qt", "data", "navigationMenu"], []),
-    links: STATIC_LINKS
+    links: STATIC_LINKS,
+    showOffcanvasMenu: state.offcanvasMenu
   };
 }
 
--- app/isomorphic/components/navigation-components/helper-components.js
+++ app/isomorphic/components/navigation-components/helper-components.js
@@ -2,17 +2,6 @@ import React from "react";
 import classNames from 'classnames';
 import { Link } from "@quintype/components";
 
-function AppLogo(props) {
-  return <div className="header__first">
-    <div className="header__first__menu" onClick={props.openSidemenu}>
-      <span className="menu-open"></span>
-    </div>
-    <div className="header__first__logo">
-      <Link href="/">{props.title}</Link>
-    </div>
-  </div>
-}
-
 function MenuItem(props) {
   const listStyle = {
     'borderColor': props.item.color
@@ -63,4 +52,4 @@ class SideMenuItem extends React.Component {
   }
 }
 
-export { SideMenuItem, SubmenuItem, MenuItem, AppLogo }
+export { SideMenuItem, SubmenuItem, MenuItem }
