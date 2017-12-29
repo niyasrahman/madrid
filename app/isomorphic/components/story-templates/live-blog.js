d630fba52f8ca0699c2207efa88ea90d50afa08a
Asim K T <asim@quintype.com>
2017-12-29 10:56:33 +0530

    [Liveblog] Fix keyevents overlapping with other stories.
    We used `postion: sticky` for this feature and removed old event
    listner/adding fixed position code. Since `sticky` is not supported in
    IE11 & Edge keyevents won't move with user scroll.

--- app/isomorphic/components/story-templates/live-blog.js
+++ app/isomorphic/components/story-templates/live-blog.js
@@ -7,43 +7,28 @@ import { LiveBlogKeyEvents } from "../story-page-components/live-blog-key-events
 import TimeAgo from 'react-timeago';
 
 class LiveBlogTemplate extends React.Component {
-  componentDidMount() {
-    if(window && window.matchMedia("(min-width: 900px)").matches) {
-      window.addEventListener('scroll', this.handleScroll);
-    }
-  }
-
-  handleScroll(){
-    const keyEvents = document.getElementsByClassName('key-events');
-    const keyEventsPos = keyEvents[0].offsetTop + 300;
-    if(window.pageYOffset > keyEventsPos){
-      keyEvents[0].classList.add("key-events__fixed");
-    } else {
-      keyEvents[0].classList.remove("key-events__fixed");
-    }
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
 
   render(){
   return <article className="live-blog-story blank-story">
       <figure className="live-blog-story-hero-image blank-story-hero-image qt-image-16x9">
         <ResponsiveImage slug={this.props.story["hero-image-s3-key"]} metadata={this.props.story["hero-image-metadata"]}
-          aspectRatio={[16,9]}
+          aspectRatio={[9,3]}
           defaultWidth={480} widths={[250,480,640]} sizes="(max-width: 500px) 98%, (max-width: 768px) 48%, 23%"
           imgParams={{auto:['format', 'compress'], fit:'max'}}/>
       </figure>
-      <div className="live-blog-story--wrapper blank-story--wrapper">
-        <div className="live-blog-story--content blank-story--content">
+      <div className="live-blog-story__extra-wrapper">
+        <div className="live-blog-story--wrapper blank-story--wrapper">
           <StoryHeaderCard story={this.props.story}/>
-          <LiveBlogKeyEvents story={ this.props.story } />
-          {this.props.story.cards.map((card, index) => <div className="story-cards" id={card.id} key={index}>
-            <TimeAgo date={card['card-added-at']}  className="card-added-at"/>
-            <StoryPageCard key={index} card={card} story={this.props.story}/>
-            <p className="card-updated-at">Last updated <TimeAgo date={card['card-updated-at']}/></p>
-          </div>)}
+        </div>
+        <LiveBlogKeyEvents story={ this.props.story } />
+        <div className="live-blog-story--wrapper blank-story--wrapper">
+          <div className="live-blog-story--content blank-story--content">
+            {this.props.story.cards.map((card, index) => <div className="story-cards" id={card.id} key={index}>
+              <TimeAgo date={card['card-added-at']}  className="card-added-at"/>
+              <StoryPageCard key={index} card={card} story={this.props.story}/>
+              <p className="card-updated-at">Last updated <TimeAgo date={card['card-updated-at']}/></p>
+            </div>)}
+          </div>
         </div>
       </div>
     </article>
