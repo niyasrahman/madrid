6a54d7e8a4448237c5200f86f9b35b939f9f0592
Asim K T <asim@quintype.com>
2017-12-29 11:50:34 +0530

    [Liveblog] Show formatted date for liveblog card.
    Included luxon as a dependency.

--- app/isomorphic/components/story-templates/live-blog.js
+++ app/isomorphic/components/story-templates/live-blog.js
@@ -5,11 +5,15 @@ import { StoryHeaderCard } from "../story-page-components/story-header-card.js";
 import { StoryPageCard } from "../story-page-components/story-page-card.js";
 import { LiveBlogKeyEvents } from "../story-page-components/live-blog-key-events.js";
 import TimeAgo from 'react-timeago';
+import { DateTime } from 'luxon';
 
 class LiveBlogTemplate extends React.Component {
+  formatter(value, unit, suffix, date, defaultFormatter) {
+    return DateTime.fromMillis(date).toFormat('dd LLL, hh:mm a');
+  }
 
   render(){
-  return <article className="live-blog-story blank-story">
+    return <article className="live-blog-story blank-story">
       <figure className="live-blog-story-hero-image blank-story-hero-image qt-image-16x9">
         <ResponsiveImage slug={this.props.story["hero-image-s3-key"]} metadata={this.props.story["hero-image-metadata"]}
           aspectRatio={[9,3]}
@@ -24,7 +28,7 @@ class LiveBlogTemplate extends React.Component {
         <div className="live-blog-story--wrapper blank-story--wrapper">
           <div className="live-blog-story--content blank-story--content">
             {this.props.story.cards.map((card, index) => <div className="story-cards" id={card.id} key={index}>
-              <TimeAgo date={card['card-added-at']}  className="card-added-at"/>
+              <TimeAgo date={card['card-added-at']}  className="card-added-at" formatter={this.formatter}/>
               <StoryPageCard key={index} card={card} story={this.props.story}/>
               <p className="card-updated-at">Last updated <TimeAgo date={card['card-updated-at']}/></p>
             </div>)}
