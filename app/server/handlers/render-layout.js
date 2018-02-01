e9b15ec11f706e2a34b6529a5672c1d457d0893e
Tejas Dinkar <tejas@gja.in>
2018-02-01 20:07:53 +0530

    Inlining fonts and css

--- app/server/handlers/render-layout.js
+++ app/server/handlers/render-layout.js
@@ -5,11 +5,14 @@ import {renderReduxComponent} from "@quintype/framework/server/render";
 import { NavigationComponent } from "../../isomorphic/components/navigation-component.js"
 import { Footer } from "../../isomorphic/components/footer.js"
 
+const cssContent = assetPath("app.css") ? readAsset("app.css") : "";
+
 export function renderLayout(res, params){
   res.render("pages/layout", _.extend({
     assetPath: assetPath,
     content: "",
     contentTemplate: null,
+    cssContent: cssContent,
     navbar: renderReduxComponent(NavigationComponent, params.store),
     footer: renderReduxComponent(Footer, params.store),
     title: "Madrid",
