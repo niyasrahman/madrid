36352bfa5da26965fcd7ef51ccc4f7cef7f00258
Jeevan Kishore <jeevan@quintype.com>
2018-02-02 15:30:37 +0530

    Change config

--- app/isomorphic/metype-config.js
+++ app/isomorphic/metype-config.js
@@ -1,7 +1,7 @@
 export const MetypeConfig = {
   accountId : 2,
   host: 'https://metype.staging.quintype.com',
-  primaryColor:'#4d086a',
+  primaryColor:'#3a9fdd',
   bgColor:'#fff',
   fontColor: '#0000',
   windowWidth: 700,
