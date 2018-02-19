c3c08b0744ebb32c046f287ee78cf40d1b9552f3
Asim KT <asim@quintype.com>
2018-02-19 15:30:59 +0530

    [GTM & GA] Remove `env` check. (#54)
    since we will use prod ID for prod only and will have our own QA in
    staging config.

--- app/isomorphic/components/gtm_id.js
+++ app/isomorphic/components/gtm_id.js
@@ -2,15 +2,13 @@ import React from 'react';
 import {connect} from "react-redux";
 
 function GTMIdProviderBase(props) {
-  // Only return a GTM id if the env is production for removing false impressions.
-  // We will show the GTM code only if an ID is present
-  return props.env === 'production' && props.publisherTheme && props.publisherTheme.gtm_id;
+  // We will show the GTM code, only if an ID is present
+  return props.publisherTheme && props.publisherTheme.gtm_id;
 }
 
 function mapStateToProps(state) {
   return {
-    publisherTheme: state.qt.config['publisher-theme'] || {},
-    env: state.qt.config['env'] || 'dev',
+    publisherTheme: state.qt.config['publisher-theme'] || {}
   }
 }
 
