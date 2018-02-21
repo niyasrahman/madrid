0f1d37de7e157cef12aa01f127462dddabfb8360
Anagh Padmanabhan <anagh.parambath@gmail.com>
2018-02-21 12:05:20 +0530

    Merge publisher and metype configs

--- app/server/data-loaders/error-page-data.js
+++ app/server/data-loaders/error-page-data.js
@@ -2,13 +2,17 @@ import _ from "lodash";
 
 import {getNavigationMenuArray} from "./menu-data";
 import {WHITELIST_CONFIG_KEYS} from "../constants";
+import MetypeConfig from "../../../config/metype-config";
+import publisher from "@quintype/framework/server/publisher-config";
 
 export function loadErrorPageData(error, config) {
   const errorComponents = { 404 : "not-found", 500: "not-found" };
   const statusCode = error.httpStatusCode || 500;
   return {
     data: { navigationMenu: getNavigationMenuArray(config.layout.menu) },
-    config: _.pick(config, WHITELIST_CONFIG_KEYS),
+    config: Object.assign(_.pick(config, WHITELIST_CONFIG_KEYS),
+            { 'publisher-theme': publisher.publisher_theme[config['publisher-name']] },
+            { 'metype-config': MetypeConfig(config['publisher-name']) }),
     pageType: errorComponents[statusCode],
     httpStatusCode: statusCode
   }
