5b068a33eb43c10b4974ade009eff031020d924c
Ravigopal Kesari <kesariravigopal@gmail.com>
2018-02-27 11:27:09 +0530

    Custom line height  (#66)
    * Set css variables for headline and body line height
    
    * Set variable values from config, with fallback values

--- app/isomorphic/components/theme.js
+++ app/isomorphic/components/theme.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import {connect} from "react-redux";
-
+import React from "react";
+import { connect } from "react-redux";
+import isEmpty from "lodash/isEmpty";
+import get from "lodash/get";
 
 function ThemeBase(props) {
+  const headlineLineHeight =
+    get(props.publisherTheme, ["headline_lineheight"]) &&
+    !isEmpty(props.publisherTheme.headline_lineheight)
+      ? props.publisherTheme.headline_lineheight
+      : "1.3";
+
+  const bodyLineHeight =
+    get(props.publisherTheme, ["body_lineheight"]) &&
+    !isEmpty(props.publisherTheme.body_lineheight)
+      ? props.publisherTheme.body_lineheight
+      : "1.5";
+
   return `.qt-theme__color {
         color: ${props.publisherTheme.primary_color} !important;
       }
@@ -16,13 +29,17 @@ function ThemeBase(props) {
         color: ${props.publisherTheme.header_text_color} !important;
       }
       .qt-theme__color--headerbg {
-        background-color: ${props.publisherTheme.header_background_color} !important;
+        background-color: ${
+          props.publisherTheme.header_background_color
+        } !important;
       }
       .qt-theme__color--footer {
         color: ${props.publisherTheme.footer_text_color} !important;
       }
       .qt-theme__color--footerbg {
-        background-color: ${props.publisherTheme.footer_background_color} !important;
+        background-color: ${
+          props.publisherTheme.footer_background_color
+        } !important;
       }
       .slick-active button::before {
         color: ${props.publisherTheme.primary_color} !important;
@@ -51,14 +68,17 @@ function ThemeBase(props) {
              background-color: ${props.publisherTheme.primary_color};
              color: #FFF;
              opacity: 0.5;
-      }`
-
+      }
+      :root {
+        --headlineLineHeight:  ${headlineLineHeight};
+        --bodyLineHeight: ${bodyLineHeight};
+      }`;
 }
 
 function mapStateToProps(state) {
   return {
-    publisherTheme: state.qt.config['publisher-theme'] || {},
-  }
+    publisherTheme: state.qt.config["publisher-theme"] || {}
+  };
 }
 
 export const Theme = connect(mapStateToProps, () => ({}))(ThemeBase);
