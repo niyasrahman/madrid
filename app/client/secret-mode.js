6ca808738e7f939e95636a106c6d7b4d896d7c6c
Ravigopal Kesari <kesariravigopal@gmail.com>
2018-03-04 22:55:46 +0530

    Adds all values of theme to secret mode
    - Use HTML5 Color Picker for colors
    - Moves the settings to center adn left side as the user can see the footer also and metype widget is not blocking
    - Supports urls for logo and monogram

--- app/client/secret-mode.js
+++ app/client/secret-mode.js
@@ -28,9 +28,39 @@ export function startSecretMode(store) {
 
 function SecretModeDialogBase({publisherTheme, updatePublisherTheme}) {
   return <div styleName="secret-mode-container">
-    Primary Color: <input type="text" value={publisherTheme['primary_color']} onChange={(e) => updatePublisherTheme('primary_color', e.target.value)}/>
-
-    Header BG Color: <input type="text" value={publisherTheme['header_background_color']} onChange={(e) => updatePublisherTheme('header_background_color', e.target.value)}/>
+    <div styleName="secret-mode-set-value">
+      <label for="set-primary-color">Primary Color </label>
+      <input id="set-primary-color" type="color" value={publisherTheme['primary_color']} onChange={(e) => updatePublisherTheme('primary_color', e.target.value)} />
+      {publisherTheme['primary_color']}
+    </div>
+    <div styleName="secret-mode-set-value">
+      <label for="set-header-bg">Header Background Color </label>
+      <input id="set-header-bg" type="color" value={publisherTheme['header_background_color']} onChange={(e) => updatePublisherTheme('header_background_color', e.target.value)} />
+      {publisherTheme['header_background_color']}
+    </div>
+    <div styleName="secret-mode-set-value">
+      <label for="set-header-text">Header Text Color </label>
+      <input id="set-header-text" type="color" value={publisherTheme['header_text_color']} onChange={(e) => updatePublisherTheme('header_text_color', e.target.value)} />
+      {publisherTheme['header_text_color']}
+    </div>
+    <div styleName="secret-mode-set-value">
+      <label for="set-footer-bg">Footer Background Color </label>
+      <input id="set-footer-bg" type="color" value={publisherTheme['footer_background_color']} onChange={(e) => updatePublisherTheme('footer_background_color', e.target.value)} />
+      {publisherTheme['footer_background_color']}
+    </div>
+    <div styleName="secret-mode-set-value">
+      <label for="set-footer-text">Footer Text Color </label>
+      <input id="set-footer-text" type="color" value={publisherTheme['footer_text_color']} onChange={(e) => updatePublisherTheme('footer_text_color', e.target.value)} />
+      {publisherTheme['footer_text_color']}
+    </div>
+    <div styleName="secret-mode-set-value">
+      <label for="set-logo">Logo </label>
+      <input id="set-logo" type="text" value={publisherTheme['logo']} onChange={(e) => updatePublisherTheme('logo', e.target.value)} placeholder="Enter the url for logo"/>
+    </div>
+    <div styleName="secret-mode-set-value">
+      <label for="set-monogram">Monogram </label>
+      <input id="set-monogram" type="text" value={publisherTheme['monogram']} onChange={(e) => updatePublisherTheme('monogram', e.target.value)} placeholder="Enter the url for monogram"/>
+    </div>
   </div>;
 }
 
