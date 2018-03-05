5a656862495cc259da09edb9fdc59ca577df094d
Tejas Dinkar <tejas@gja.in>
2018-03-05 17:15:48 +0530

    Adding icon support to manifest

--- app/server/manifest-fn.js
+++ app/server/manifest-fn.js
@@ -3,7 +3,18 @@ import publisher from "@quintype/framework/server/publisher-config";
 export function manifestFn(config) {
   const theme = publisher.publisher_theme[config['publisher-name']] || {};
 
-  return Promise.resolve({
+  const manifest = {
     "theme_color": theme.primary_color || "#2f73e4"
-  });
+  };
+
+  if(theme['amp_logo']) {
+    manifest.icons = [
+      Object.assign({
+        type: "image/png",
+        sizes: "512x512"
+      }, theme['amp_logo'])
+    ];
+  }
+
+  return Promise.resolve(manifest);
 }
