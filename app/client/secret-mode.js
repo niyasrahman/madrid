6eee1b68b126bd57af12bea8911fe2baaea3f452
Tejas Dinkar <tejas@gja.in>
2018-03-05 16:42:58 +0530

    Adding the download button

--- app/client/secret-mode.js
+++ app/client/secret-mode.js
@@ -4,6 +4,7 @@ import React from 'react';
 import {connect, Provider} from 'react-redux';
 import './secret-mode.module.css';
 import {Theme} from '../isomorphic/components/theme'
+import FileSaver from 'file-saver';
 
 export function startSecretMode(store) {
   console.log("Starting Secret Mode");
@@ -23,46 +24,53 @@ export function startSecretMode(store) {
   ReactDom.render(<Provider store={store}><Theme /></Provider>, document.getElementById("theme-css"));
 }
 
-function SecretModeDialogBase({publisherTheme, updatePublisherTheme}) {
-  return <div styleName="secret-mode-container">
-    <div styleName="secret-mode-set-value">
+function SecretModeDialogBase({publisherTheme, updatePublisherTheme, onDownload}) {
+  return <div styleName="container">
+    <div styleName="set-value">
       <label for="set-primary-color">Primary Color </label>
       <input id="set-primary-color" type="color" value={publisherTheme['primary_color']} onChange={(e) => updatePublisherTheme('primary_color', e.target.value)} />
       {publisherTheme['primary_color']}
     </div>
-    <div styleName="secret-mode-set-value">
+    <div styleName="set-value">
       <label for="set-header-bg">Header Background Color </label>
       <input id="set-header-bg" type="color" value={publisherTheme['header_background_color']} onChange={(e) => updatePublisherTheme('header_background_color', e.target.value)} />
       {publisherTheme['header_background_color']}
     </div>
-    <div styleName="secret-mode-set-value">
+    <div styleName="set-value">
       <label for="set-header-text">Header Text Color </label>
       <input id="set-header-text" type="color" value={publisherTheme['header_text_color']} onChange={(e) => updatePublisherTheme('header_text_color', e.target.value)} />
       {publisherTheme['header_text_color']}
     </div>
-    <div styleName="secret-mode-set-value">
+    <div styleName="set-value">
       <label for="set-footer-bg">Footer Background Color </label>
       <input id="set-footer-bg" type="color" value={publisherTheme['footer_background_color']} onChange={(e) => updatePublisherTheme('footer_background_color', e.target.value)} />
       {publisherTheme['footer_background_color']}
     </div>
-    <div styleName="secret-mode-set-value">
+    <div styleName="set-value">
       <label for="set-footer-text">Footer Text Color </label>
       <input id="set-footer-text" type="color" value={publisherTheme['footer_text_color']} onChange={(e) => updatePublisherTheme('footer_text_color', e.target.value)} />
       {publisherTheme['footer_text_color']}
     </div>
-    <div styleName="secret-mode-set-value">
+    <div styleName="set-value">
       <label for="set-logo">Logo </label>
       <input id="set-logo" type="text" value={publisherTheme['logo']} onChange={(e) => updatePublisherTheme('logo', e.target.value)} placeholder="Enter the url for logo"/>
     </div>
-    <div styleName="secret-mode-set-value">
+    <div styleName="set-value">
       <label for="set-monogram">Monogram </label>
       <input id="set-monogram" type="text" value={publisherTheme['monogram']} onChange={(e) => updatePublisherTheme('monogram', e.target.value)} placeholder="Enter the url for monogram"/>
     </div>
+    <input type="submit" value="Download Config" onClick={() => onDownload()} styleName="submit" />
   </div>;
 }
 
 const SecretModeDialog = connect(state => ({
-  publisherTheme: state.qt.config["publisher-theme"]
+  publisherTheme: state.qt.config["publisher-theme"],
+  onDownload: function() {
+    const blob = new Blob([JSON.stringify({
+      "publisher-theme": state.qt.config["publisher-theme"]
+    })], {type: "application/json;charset=utf-8"});
+    FileSaver.saveAs(blob, "config.json");
+  }
 }), dispatch => ({
   updatePublisherTheme: (field, value) => dispatch({type: "SECRET_MODE", update: {qt: {config: {"publisher-theme": {[field]: {$set: value}}}}}})
 }))(SecretModeDialogBase)
