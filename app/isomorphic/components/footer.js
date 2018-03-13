d03c046aef84ce6044974b13727c9b314e163a5a
shreyaqt <shreya@quintype.com>
2018-03-13 09:42:56 +0530

    adds footer

--- app/isomorphic/components/footer.js
+++ app/isomorphic/components/footer.js
@@ -6,41 +6,71 @@ import indexOf from "lodash/indexOf";
 import assetify from '@quintype/framework/assetify';
 import Logo from '../../assets/icons/quintype_logo.svg';
 import { STATIC_LINKS } from "./constants";
+import { FbIcon } from "./layout/social/facebook-icons.js";
+import { TwitterIcon }  from "./layout/social/twitter-icon.js";
+import { YoutubeIcon } from "./layout/social/youtube.js";
+import { GooglePlus } from "./layout/social/google-plus.js";
 
 function FooterBase(props) {
   const copyrightText = get(props.publisherSettings, ['copyright'], '');
-
-  return <footer className="footer qt-theme__color--footerbg qt-theme__color--footer">
-    <div className="footer__content">
-      <div className="footer__content__first">
-        <img className="app-logo__element" src={props.publisherTheme.monogram} alt={props.publisherName}/>
-      </div>
-      <div className="footer__content__last">
-        <ul>
-          {props.links.map((item, index) => {
-            return <li key={index}><a href={item.url}>{item.content}</a></li>
-          })}
-        </ul>
-        <div className="copy--right">
-          <div>{copyrightText}</div>
-          <a href="https://www.quintype.com/" target="_blank"><strong>Powered by quintype</strong></a>
-        </div>
-      </div>
-    </div>
-  </footer>
+console.log(props);
+  return (
+  <footer className="footer-part">
+    <SocialLinks socialLinks={props.socialLinks}/>
+    <FooterLinks/>
+   </footer>
+   );
 }
+
+function SocialLinks(props) {
+return(
+     <div className="social-links">
+        <a href={props.socialLinks['facebook-url']}><FbIcon width={50} height={50}/></a>
+        <a href={props.socialLinks['facebook-url']}><TwitterIcon width={50} height={50}/></a>
+        <a href={props.socialLinks['facebook-url']}><YoutubeIcon width={50} height={50}/></a>
+        <a href={props.socialLinks['google-plus-url']}><GooglePlus width={50} height={50}/></a>
+      </div>
+
+  );
+}
+
+
+
+
+function FooterLinks(props) {
+return(
+    <div className="footer-links">
+    <div className="footer-links">
+      <ul className="link-list">
+          <li className="link-list__item"><a href="https://www.quintype.com/" target="_blank">About</a></li>
+          <li className="link-list__item"><a href="https://www.quintype.com/" target="_blank">people</a></li>
+          <li className="link-list__item"><a href="https://www.quintype.com/" target="_blank">jobs</a></li>
+          <li className="link-list__item"><a href="https://www.quintype.com/" target="_blank">contact</a></li>
+       </ul>
+    </div>
+    <div className="copyright">
+      <a href="https://www.quintype.com/" target="_blank"><strong>&copy Powered by quintype</strong></a>
+    </div>
+    </div>
+
+  );
+}
+
 
 function mapStateToProps(state) {
   const validStaticPages = state.qt.config['publisher-theme']['static_pages'];
   const updatedStaticLinks = filter(STATIC_LINKS, (link) => {
     return indexOf(validStaticPages, link.templateKey) > -1;
   });
+
+  const socialLinks = state.qt.config['social-links'];
   return {
     links: updatedStaticLinks,
-    publisherTheme: state.qt.config['publisher-theme'] || {},
-    publisherSettings: state.qt.config['publisher-settings'] || {},
-    publisherName: state.qt.config['publisher-name'] || ''
+    socialLinks: socialLinks
   }
 }
 
+
+export { SocialLinks };
+export { FooterLinks };
 export const Footer = connect(mapStateToProps, () => ({}))(FooterBase);
